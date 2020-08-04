e0887c6908037349a9ab1ccd112ddf0f962b779c
HenCor2019 <Henry200amaya@gmail.com>
2020-08-03 18:46:02 -0600

    Fix: Indent lines

--- Partials/partial02.py
+++ Partials/partial02.py
@@ -2,15 +2,15 @@ import random as rnd
 import numpy as np
 
 def Options() :
-option = "1. Agregar\n2. Buscar\n.Salir\n"
-print(option)
-op = int(input(opcion:))
+ option = "1. Agregar\n2. Buscar\n.Salir\n"
+ print(option)
+ op = int(input("opcion: "))
 
 def Create_Student() :
-name = input("nombre: ")
-s_name = np.append(s_name,name)
-carnet = "00"+ str(rnd.randint(1000,9999)) + "20"
-s_carnet = np.append(s_carnet,carnet)
+ name = input("nombre: ")
+ s_name = np.append(s_name,name)
+ carnet = "00"+ str(rnd.randint(1000,9999)) + "20"
+ s_carnet = np.append(s_carnet,carnet)
 
 #muestra los datos del estudiante
 def Show_Student(s) :
@@ -24,13 +24,13 @@ s_name = np.array([])
 s_carnet = np.array([])
 
 while True :
-if Options() == 1 :
- Create_Student()
-elif Options() == 2 :
- tmp_student = input("estudiante: ")
- Show_Student(tmp_student)
-elif Options() == 3 :
- print("Hasta la proxima!!")
- break
-else :
-print("opcion erronea!!")
+ if Options() == 1 :
+  Create_Student()
+ elif Options() == 2 :
+  tmp_student = input("estudiante: ")
+  Show_Student(tmp_student)
+ elif Options() == 3 :
+  print("Hasta la proxima!!")
+  break
+ else :
+  print("opcion erronea!!")
