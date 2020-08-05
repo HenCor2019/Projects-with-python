2e863b3463cd3deb05ba2a419c22822a35f9a136
HenCor2019 <Henry200amaya@gmail.com>
2020-08-05 10:50:30 -0600

    Fix: Function Options fix

--- Partials/partial02.py
+++ Partials/partial02.py
@@ -1,16 +1,26 @@
 import random as rnd
 import numpy as np
 
+#variables
+s_name = np.array([])
+s_carnet = np.array([])
+flag = True
+op = 0
+
+#opciones que puede hacer el usuario
 def Options() :
- option = "1. Agregar\n2. Buscar\n.Salir\n"
+ option = "1. Agregar\n2. Buscar\n3. Salir\n"
  print(option)
  op = int(input("opcion: "))
+ return op
 
-def Create_Student() :
+#retorna la tupla de valores de carnet y estudiante
+def Create_Student(names,carnets) :
  name = input("nombre: ")
- s_name = np.append(s_name,name)
+ names = np.append(names,name)
  carnet = "00"+ str(rnd.randint(1000,9999)) + "20"
- s_carnet = np.append(s_carnet,carnet)
+ carnets = np.append(carnets,carnet)
+ return names,carnets
 
 #muestra los datos del estudiante
 def Show_Student(s) :
@@ -19,18 +29,16 @@ def Show_Student(s) :
      print("estudiante: " + str(student))
      print("carnet: " + str(carnet))
 
-#variables
-s_name = np.array([])
-s_carnet = np.array([])
+while flag :
+ op = Options() 
 
-while True :
- if Options() == 1 :
-  Create_Student()
- elif Options() == 2 :
+ if op == 1 : #agregar estudiante
+  s_name,s_carnet = Create_Student(s_name,s_carnet)
+ elif op == 2 : # buscar estudiante
   tmp_student = input("estudiante: ")
   Show_Student(tmp_student)
- elif Options() == 3 :
+ elif op == 3 : # salir del programa
   print("Hasta la proxima!!")
-  break
+  flag = False 
  else :
   print("opcion erronea!!")
