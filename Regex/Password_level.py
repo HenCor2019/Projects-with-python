349d4b46fb17891e45d079ebcfa661d9245f3985
HenCor2019 <Henry200amaya@gmail.com>
2020-08-07 19:21:44 -0600

    Add : Regex applied to passwords

---
+++ Regex/Password_level.py
@@ -0,0 +1,62 @@
+import re
+
+password = input("costrase?a : ")
+
+
+'''
+debiles : solo menos de ocho alfanumericos 
+ejemplo : 123abcde
+
+intermedios : contras debiles que cuentan con al menos cuatro letras mayusculas
+ejemplo : 123ABCDe
+
+fuertes : contras intermedias;que cuentan con al menos dos simbolos
+ejemplo : #123ABCDe=$
+cant
+'''
+
+#variables 
+ilicit_word = "pene|vagina|puto|pendejo"
+ilicit_pattern = re.compile(ilicit_word)
+
+simbols_password = False
+
+alpha_numeric_password = False
+
+capital_letter_password = False
+
+password_level = "vulnerable"
+
+quantity_simbols = (lambda p : len(re.findall("\W",p))) (password)
+
+quantity_alpha_num = (lambda p : len(re.findall("\w",p)))(password)
+
+quantity_capital_letter = (lambda p : len(re.findall("[A-Z]",p)))(password)
+
+if re.search("\s",password) is None : 
+ if ilicit_pattern.search(password) is None : 
+  
+  #caso debil
+   if quantity_alpha_num >= 7 : 
+   
+       password_level = "debil"
+
+  #caso intermedio
+   if quantity_alpha_num >= 7 and quantity_capital_letter >= 4 :  
+
+       password_level = "intermedia"
+ 
+   #caso fuerte 
+   if quantity_alpha_num >= 7 and quantity_capital_letter >= 4 and quantity_simbols >= 2 : 
+ 
+       password_level = "fuerte"
+
+ 
+   print("El nivel de tu contrase?a es : " + str(password_level))
+
+ else :
+   print("Algunas palabras no son admitidas")
+
+else : 
+ print("No puedes agregar espacios")
+
