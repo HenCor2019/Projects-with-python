b605206a31d8d7e2ec1db640bc4baa51877bbe28
HenCor2019 <Henry200amaya@gmail.com>
2020-10-09 21:00:26 -0600

    add: product

---
+++ product.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+array5I = np.array([])
+def Function_5i(x,a) :
+    tmp = 1
+    while tmp <= x :
+        a= np.append(a,5*tmp)
+        tmp = tmp + 1
+    
+    print(a)
+    print(a.prod())
+    return a.prod()
+
+def Function_5i5i(x,a) :
+    tmp = 1
+    while tmp <= x :
+        a= np.append(a,5*tmp + 5)
+        tmp = tmp + 1
+    
+    print(a)
+    print(a.prod())
+    return a.prod()
+
+print(Function_5i(100,array5I)/Function_5i5i(100,array5I))
+
