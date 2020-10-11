330ea5be216fcfbc7064de10a958e6d8bf48c5ec
Henry Cortez <54414736+HenCor2019@users.noreply.github.com>
2020-10-10 18:45:24 -0600

    Update: product

--- product.py
+++ product.py
@@ -3,17 +3,20 @@ import numpy as np
 
 array5I = np.array([])
 def Function_5i(x,a) :
+    print("Hola, modificando")
     tmp = 1
     while tmp <= x :
         a= np.append(a,5*tmp)
         tmp = tmp + 1
     
     print(a)
+    print("Hola, esto es de github")
     print(a.prod())
     return a.prod()
-
+    
 def Function_5i5i(x,a) :
-    tmp = 1
+    tmp = 2
+    tmp = 2- 1
     while tmp <= x :
         a= np.append(a,5*tmp + 5)
         tmp = tmp + 1
@@ -22,5 +25,6 @@ def Function_5i5i(x,a) :
     print(a.prod())
     return a.prod()
 
+print("Imprimi una funcion")
 print(Function_5i(100,array5I)/Function_5i5i(100,array5I))
 
