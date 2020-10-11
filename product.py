6d164904848f7f4058ff67d15e5c39452afed6e1
HenCor2019 <Henry200amaya@gmail.com>
2020-10-10 18:47:44 -0600

    Up: product desde telefono

--- product.py
+++ product.py
@@ -3,12 +3,13 @@ import numpy as np
 
 array5I = np.array([])
 def Function_5i(x,a) :
+    tmp = 10
     tmp = 1
+    tmp = 2
     while tmp <= x :
         a= np.append(a,5*tmp)
         tmp = tmp + 1
     
-    print(a)
     print(a.prod())
     return a.prod()
 
