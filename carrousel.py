cc8627e8cae0943de6f4d6021f9f0aa8aaf5fdab
HenCor2019 <Henry200amaya@gmail.com>
2020-11-15 20:24:03 -0600

    Add: commit with vim fugitive

--- carrousel.py
+++ carrousel.py
@@ -1,19 +1,19 @@
 persons_ages = []
-pers_age = 0
+person_age = 0
 i = 0
 
 while i<3:
-    pers_age = int(input("Algo :"))
-    persons_ages.append(pers_age)
+    person_age = int(input("Algo :"))
+    persons_ages.append(person_age)
     i+=1
- 
+
 #tu código va aquí
-def Validahion_Age(Ages) :
+def Validation_Ages(Ages) :
     for age in Ages :
        if age < 16:
           return 'Too young!'
     return 'Get ready!'
 
-validation = Validahion_Age(persons_ages)
+validation = Validation_Ages(persons_ages)
 
 print(validation)
